2bf783ef63315d6441ce09690b10758699c7253e
Cam Gorrie <sastraxi@gmail.com>
2023-06-21 16:20:48 -0400

    ez lookup of chords

--- src/prompts/ChordProgressionPrompt.tsx
+++ src/prompts/ChordProgressionPrompt.tsx
@@ -4,6 +4,10 @@ import IconButton from '../components/IconButton'
 import Choice from '../components/Choice'
 import ChordDiagram from '../components/ChordDiagram'
 
+// N.B. from https://github.com/tombatossals/chords-db/blob/master/lib/guitar.json
+import GuitarChords from '../theory/guitar.json'
+import { ChordDefinition } from 'vexchords'
+
 import './ChordProgressionPrompt.css'
 
 type ChordLibraryEntry = {
@@ -20,27 +24,58 @@ type Fretting = {
   barres: number[]
 }
 
-// N.B. from https://github.com/tombatossals/chords-db/blob/master/lib/guitar.json
-import GuitarChords from '../theory/guitar.json'
-import { ChordDefinition, GuitarString } from 'vexchords'
+/**
+ * Normalize the root of a chord for lookup in the database.
+ * This is based on the published "keys" in guitar.json
+ */
+const translateKeyMap: Record<string, string> = {
+  'Db': 'C#',
+  'D#': 'Eb',
+  'Gb': 'F#',
+  'G#': 'Ab',
+  'A#': 'Bb',
+}
+
+// we sort the keys in order of descending length to match "Eb" before "E"
+const allKeysInDescLength = [...GuitarChords.keys, ...Object.keys(translateKeyMap)]
+allKeysInDescLength.sort((a, b) => b.length - a.length)
+
+/**
+ * Gets a root and a suffix for lookup in the guitar chords database.
+ * @param chordName descriptive chord name, e.g. "A#minor"
+ * @returns { root, suffix } e.g. { root: "Bb", suffix: "minor" }
+ */
+const getRootAndSuffix = (chordName: string) => {
+  let root, suffix
+  for (const prefix of allKeysInDescLength) {
+    if (chordName.startsWith(prefix)) {
+      if (Object.prototype.hasOwnProperty.call(translateKeyMap, prefix)) {
+        root = translateKeyMap[prefix]
+      } else {
+        root = prefix
+      }
+      suffix = chordName.substring(prefix.length).trim()
+      return { root, suffix }
+    }
+  }
+  throw new Error(`Could not find root for chord name: ${chordName}`)
+}
 
 /**
  * Looks up a guitar chord based on chord name in chords-db.
- * @param chord the chord name, e.g. C/D#, Emmaj7b5, F major
+ * @param chordName the chord name, e.g. C/D#, Emmaj7b5, F major
  * @param variant 
  * @returns 
  */
-const getFretting = (chord: string, variant?: number): Fretting => {
-  const root = GuitarChords.keys.find(k => chord.startsWith(k))
-  if (!root) {
-    throw new Error(`Could not find root for chord name: ${chord}`)
-  }
-  const suffix = chord.substring(root.length).trim()
-  const allSuffixes: Array<ChordLibraryEntry> = (GuitarChords.chords as Record<string, any>)[root]
+const getFretting = (chordName: string, variant?: number): Fretting => {
+  const { root, suffix } = getRootAndSuffix(chordName)
+
+  const lookupKey = root.replace("#", "sharp")  // who knows!
+  const allSuffixes: Array<ChordLibraryEntry> = (GuitarChords.chords as Record<string, any>)[lookupKey]
   const frettings: Array<Fretting> | undefined = allSuffixes.find(x => x.suffix === suffix)?.positions
   if (!frettings) {
     console.log('Available suffixes:', allSuffixes.map(x => x.suffix))
-    throw new Error(`Could not find frettings for ${chord}`)
+    throw new Error(`Could not find ${root} frettings for ${suffix}`)
   }
 
   return frettings[(variant ?? 0) % frettings.length]
@@ -50,6 +85,7 @@ const frettingToVexChord = (f: Fretting): ChordDefinition => {
   return {
     chord: f.frets.map((n, fretIndex) => [6 - fretIndex, (n === -1 ? 'x' : n)]),
     position: f.baseFret,
+    // TODO: barres
   }
 } 
 
@@ -59,7 +95,7 @@ const ChordProgressionPrompt: React.FunctionComponent = () => {
     "B minor",
     "C major",
     "D major",
-    "G maj7"
+    "Gb m7b5"
   ]
 
   const frettings = CHORDS.map(chord => getFretting(chord))
